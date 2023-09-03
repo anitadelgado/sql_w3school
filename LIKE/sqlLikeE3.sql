b18c2a9598f168303198e863c1f989df51b9b34a
Ana Delgado <123459805+anitadelgado@users.noreply.github.com>
2023-09-03 18:06:53 +0200

    Add files via upload

---
+++ LIKE/sqlLikeE3.sql
@@ -0,0 +1,18 @@
+/**
+ *	from SQL Like: Exercise 3 ( https://www.w3schools.com/sql/exercise.asp?filename=exercise_like3 )
+ *
+ *	question:
+ *	Select all records where the value of the City column contains the letter "a".
+ *
+ *	SELECT * FROM Customers
+ *	_____________________;
+ *
+ */
+SELECT * FROM Customers
+WHERE City LIKE '%a%';
+
+/* MS ACCESS version */
+/*
+SELECT * FROM Customers
+WHERE City LIKE '*a*';
+*/
